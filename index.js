d942c868f6ccfc48a4478a5b6920f9ef3a34626f
Chaimcv <chaithanyamcv@gmail.com>
2025-12-11 22:23:49 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const product=fetch('https://fakestoreapi.com/products/')
+           .then(res=>res.json())
+             .then(product=>{
+              console.log(product);
+    const display=product.map(item=>{             
+    return`
+    <div class="card"{key=item.id}>
+    <h3 class="card-title">${item?.title}</h3>
+   
+    <img class="image" src="${item?.image}"> 
+     <h4 class="card-price">Price:${item?.price}</p>
+    </div>
+      `;}).join("");
+       document.getElementById("display").innerHTML=display;
+       
+       })
+  
+    
+       
