5ff88e037c876204e1a191300dabd08aceb9b93e
Chaimcv <chaithanyamcv@gmail.com>
2025-12-11 22:45:24 +0530

    Updates

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const product=fetch('https://fakestoreapi.com/products/')
     <div class="card"{key=item.id}>
     <h3 class="card-title">${item?.title}</h3>
     <img class="image" src="${item?.image}"> 
-     <h4 class="card-price">Price:${item?.price}</p>
+     <h4 class="card-price">Price:${item?.price}</h4>
+     <p class="card-description">${item?.description}</p>
     </div>
       `;}).join("");
        document.getElementById("display").innerHTML=display;
